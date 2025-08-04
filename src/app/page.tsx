adc2e45c64e087a9b185b39f629b5433df60fd69
elenbrodt <elenbrodt@gmail.com>
2025-08-04 15:43:44 -0300

    teste com favicon e width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans items-center justify-center h-screen p-4 pb-20 gap-16 min-w-2xl">
+      <main className="flex flex-col gap-[32px] items-center justify-center">
         <Image
           src="/zoom-logo.png"
-          alt="Next.js logo"
+          alt="Zoom logo"
           width={180}
           height={38}
           priority
@@ -14,10 +14,11 @@ export default function Home() {
         <div>
           <iframe
             title="Simulador RT - Simples Nacional v1.1"
-            width="1140"
-            height="541.25"
+            width="100%"
+            height="100%"
             src="https://app.powerbi.com/reportEmbed?reportId=bdf168cb-54a7-4354-bda7-d8ae57f4581a&autoAuth=true&ctid=09c84ef1-b748-483c-9a33-9e33fbda6ee3"
-            allowFullScreen
+            allowFullScreen={true}
+            className="w-[80vw] h-[80vh]"
           ></iframe>
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row"></div>
